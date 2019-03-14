9f56fd92e5c42e88bce444d3f337308a67d46c1b
Audra Tallman <audra.tallman@hotmail.com>
2019-03-14 21:27:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/\w+^[aeiouAEIOU]/) ? true :false
+  word.match(/\w+^[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\w^[A...Z]$\W)
+  text.match(/\w^[A...Z]+ing$) ? true : false
 end
 
 def valid_phone_number?(phone)
